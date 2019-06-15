3cd7560af90020a727447df0fbe6f4088358ac4a
Laura-Neff <ljneff@gmail.com>
2019-06-14 23:57:42 -0400

    Final changes

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,5 @@
 
-  // Your web app's Firebase configuration
+
   var firebaseConfig = {
     apiKey: "AIzaSyB3vl5FsOXJKAYOf6RWTE5nfHqqAu8acp8",
     authDomain: "trainscheduler-deac4.firebaseapp.com",
@@ -9,20 +9,19 @@
     messagingSenderId: "1022981038374",
     appId: "1:1022981038374:web:8af1e142e2f9f794"
   };
-  // Initialize Firebase
+
   firebase.initializeApp(firebaseConfig);
   var database = firebase.database();
 
   $("#addTrain").click(function(event) {
     event.preventDefault();
 
-    // Grabs user input
     var trainInput = $("#trainName").val().trim();
     var destinationInput = $("#destination").val().trim();
     var timeInput = moment($("#firstTime").val().trim(),'HH:mm').format("hh:mm a");
     var frequencyInput = $("#frequency").val().trim();
 
-    // Creates local "temporary" object for holding train data
+    
     var newTrain = {
         train: trainInput,
         dest: destinationInput,
@@ -30,10 +29,10 @@
         freq: frequencyInput
     };
     
-     // Uploads employee data to the database
+     
      database.ref().push(newTrain);
 
-     // Logs everything to console
+     
      console.log(newTrain.train);
      console.log(newTrain.dest);
      console.log(newTrain.time);
@@ -41,7 +40,7 @@
  
      alert("Train added");
  
-     // Clears all of the text-boxes
+     
      $("#trainName").val("");
      $("#destination").val("");
      $("#firstTime").val("");
@@ -69,14 +68,13 @@
     console.log(firstArrivalConvert);
 
 
-    // Current Time
+
     var now = moment();
     console.log("CURRENT TIME: " + moment().format("hh:mm a"));
 
 
     var theDifference = moment().diff(moment(firstArrivalConvert), "minutes");
-    // var diffFormat = moment(theDifference).format("hh:mm");
-
+    
     console.log("DIFFERENCE IN TIME: " + theDifference);
     console.log(moment(theDifference).format("hh:mm"));
 
@@ -84,11 +82,11 @@
     var timeInterval = theDifference % frequencyInput;
     console.log(timeInterval);
 
-    // Minute Until Train
+  
     var minutesLeft = frequencyInput - timeInterval;
     console.log("MINUTES TILL TRAIN: " + minutesLeft);
 
-    // Next Train
+    
     var nextArrival = moment().add(minutesLeft, "minutes");
     var nextArrivalSimplified =  moment(nextArrival).format("hh:mm a");
     console.log("ARRIVAL TIME: " + nextArrivalSimplified);
@@ -97,22 +95,17 @@
 
 
 
-    // Calculate the total billed rate
-    // var empBilled = empMonths * empRate;
-    // console.log(empBilled);
-
-    // Create the new row
+  
     var newRow = $("<tr>").append(
         $("<td>").text(trainInput),
         $("<td>").text(destinationInput),
         $("<td>").text(frequencyInput),
         $("<td>").text(nextArrivalSimplified),
         $("<td>").text(minutesLeft),
-        // $("<td>").text(empRate),
-        // $("<td>").text(empBilled)
+        
     );
 
-    // Append the new row to the table
+
     $("#train-table > tbody").append(newRow);
 });
 
